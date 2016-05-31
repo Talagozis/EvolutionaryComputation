51ca2b942b0036b91402d625c9ffb258819e2798
Talagozis <talagozis@hotmail.com>
2016-05-31 10:30:37 +0300

    FirstOrDefault()

---
+++ EvolutionaryComputation/Models/Person.cs
@@ -0,0 +1,64 @@
+﻿using EvolutionaryComputation.App_Code;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EvolutionaryComputation.Models
+{
+    public class Person
+    {
+        public Person()
+        {
+            
+        }
+        public Person(int bits)
+        {
+            for (int j = 0; j < bits; j++)
+            {
+                if (Core.rnd < 0.5)
+                    dna += "0";
+                else
+                    dna += "1";
+            }
+        }
+        public string dna { get; set; }
+        public double fitness
+        {
+            get
+            {
+                double x = geno2Pheno;
+                return Core.function(x);
+            }
+        }
+
+        public int str2Int
+        {
+            get        
+            {
+                int i, ak = 0, pow2 = 1 << (dna.Length - 1);
+                for (i = 1; i < dna.Length; i++)
+                {
+                    if (dna[i] == '1') ak += pow2;
+                    pow2 /= 2;
+                }
+                return ak;
+            }
+        }
+
+        public double geno2Pheno
+        {
+            get        
+            {
+                int ak = this.str2Int;
+                return (double)ak/(Math.Pow(2, dna.Length)-1)* Math.PI * 1d;
+            }
+        }
+
+        
+
+        
+
+    }
+
+}
---
+++ EvolutionaryComputation/Windows/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using EvolutionaryComputation.App_Code;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.DataVisualization.Charting;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace EvolutionaryComputation.Windows
+{
+    public partial class MainWindow : Window
+    {
+        private Core core;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            core = new Core();            
+        }
+
+        private void buttonInitialize_Click(object sender, RoutedEventArgs e)
+        {
+            Cursor = Cursors.Wait;
+
+            int numberOfPopulation = int.Parse(textBoxPopulation.Text);
+            int bits = int.Parse(textBoxBits.Text);
+            double mutation = double.Parse(textBoxMutation.Text);
+            double noCrossover = double.Parse(textBoxNoCrossover.Text);
+            double parameterA = double.Parse(textBoxParameterA.Text);
+            double parameterB = double.Parse(textBoxParameterB.Text);
+            var parents = this.core.Initialize(numberOfPopulation, bits, mutation, noCrossover, parameterA, parameterB);
+            this.DataContext = core;
+            this.textBoxGenerations.Text = this.core.generations.ToString();
+
+            Cursor = Cursors.Arrow;
+        }
+
+        private void buttonNext_Click(object sender, RoutedEventArgs e)
+        {
+            Cursor = Cursors.Wait;
+
+            int steps = int.Parse(textBoxSteps.Text);
+            this.core.Next(steps);
+
+            this.textBoxGenerations.Text = this.core.generations.ToString();
+
+            Cursor = Cursors.Arrow;
+        }
+    }
+}
